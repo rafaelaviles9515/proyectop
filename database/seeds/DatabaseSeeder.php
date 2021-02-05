4507347ac6df19d1ad4d6e37c6bc40c76cbc820b
Rafael <proyectorafael123@gmail.com>
2021-02-05 11:22:11 -0600

    agregando seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Categoria;
+use App\Models\Estado;
+use App\Models\Rol;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       $categoria=new Categoria;
+       $categoria->nombre='Miedo';
+       $categoria->save();
+
+       $categoria=new Categoria;
+       $categoria->nombre='Suspenso';
+       $categoria->save();
+
+       $categoria=new Categoria;
+       $categoria->nombre='Accion';
+       $categoria->save();
+
+       $categoria=new Estado;
+       $categoria->id=1;
+       $categoria->nombre='Disponible';
+       $categoria->save();
+
+       $categoria=new Estado;
+       $categoria->id=2;
+       $categoria->nombre='No Disponible';
+       $categoria->save();
+
+       $categoria=new Estado;
+       $categoria->id=3;
+       $categoria->nombre='Reservado';
+       $categoria->save();
+
+       $categoria=new Estado;
+       $categoria->id=4;
+       $categoria->nombre='Pagado';
+       $categoria->save();
+
+       $rol=new Rol;
+       $rol->id=1;
+       $rol->nombre='Administrador';
+       $rol->save();
+
+       $rol=new Rol;
+       $rol->id=2;
+       $rol->nombre='Cliente';
+       $rol->save();
+
+       $usuario=new User;
+       $usuario->name='Administrador';
+       $usuario->email='administrador@gmail.com';
+       $usuario->rol_id=1;
+       $usuario->saldo=1000;
+       $usuario->password = bcrypt('12345678');
+       $usuario->save();
+
+
     }
 }
